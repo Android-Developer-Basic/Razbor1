e6cc9919243dab6f0949b58c9ddcaa7b0eeddf75
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-04-26 13:15:41 +0200

    Decorator

--- src/main/kotlin/ru/otus/homework/decorator.kt
+++ src/main/kotlin/ru/otus/homework/decorator.kt
@@ -3,8 +3,9 @@ package ru.otus.homework
 fun main() {
 
     val order = mutableListOf<ReceiptItem>()
-    order.add(CoffeeWithMilk())
-    order.add(CoffeeWithMilkAndSugar())
+    order.add(Milk(Coffee()))
+    order.add(Sugar(Milk(Coffee())))
+    order.add(Milk(Tea()))
 
     println("Thank you!")
     var total = 0.0
@@ -25,22 +26,14 @@ open class Coffee : ReceiptItem {
     override fun getPrice(): Double = 1.0
 }
 
-open class CoffeeWithMilk : Coffee() {
-    override fun getName(): String = super.getName() + " + milk"
-    override fun getPrice(): Double = super.getPrice() + 0.5
+class Milk(private val item: ReceiptItem) : ReceiptItem {
+    override fun getName(): String = item.getName() + " + milk"
+    override fun getPrice(): Double = item.getPrice() + 0.5
 }
 
-open class CoffeeWithSugar : Coffee() {
-    override fun getName(): String = super.getName() + " + sugar"
-    override fun getPrice(): Double = super.getPrice() + 0.2
-}
-
-// Проблемы с наследованием!!!
-// Мы не можем создать класс, который наследует CoffeeWithMilk и CoffeeWithSugar
-// и добавляем сахар, повторяя класс CoffeeWithSugar
-class CoffeeWithMilkAndSugar : CoffeeWithMilk() {
-    override fun getName(): String = super.getName() + " + sugar"
-    override fun getPrice(): Double = super.getPrice() + 0.2
+class Sugar(private val item: ReceiptItem) : ReceiptItem {
+    override fun getName(): String = item.getName() + " + sugar"
+    override fun getPrice(): Double = item.getPrice() + 0.2
 }
 
 class Tea : ReceiptItem {
