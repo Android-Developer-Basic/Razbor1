a9843a48b8e596e8535e559a6b0f7607e28ee603
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-04-26 13:29:08 +0200

    Decorator Kotlin way :)

--- src/main/kotlin/ru/otus/homework/decorator.kt
+++ src/main/kotlin/ru/otus/homework/decorator.kt
@@ -3,9 +3,9 @@ package ru.otus.homework
 fun main() {
 
     val order = mutableListOf<ReceiptItem>()
-    order.add(Milk(Coffee()))
-    order.add(Sugar(Milk(Coffee())))
-    order.add(Milk(Tea()))
+    order.add(Coffee() + Milk)
+    order.add(Coffee() + Milk + Sugar)
+    order.add(Tea() + Milk)
 
     println("Thank you!")
     var total = 0.0
@@ -19,6 +19,12 @@ fun main() {
 interface ReceiptItem {
     fun getName(): String
     fun getPrice(): Double
+
+    operator fun plus(factory: DecoratorFactory): ReceiptItem = factory.decorate(this)
+}
+
+interface DecoratorFactory {
+    fun decorate(item: ReceiptItem): ReceiptItem
 }
 
 open class Coffee : ReceiptItem {
@@ -29,11 +35,19 @@ open class Coffee : ReceiptItem {
 class Milk(private val item: ReceiptItem) : ReceiptItem {
     override fun getName(): String = item.getName() + " + milk"
     override fun getPrice(): Double = item.getPrice() + 0.5
+
+    companion object : DecoratorFactory {
+        override fun decorate(item: ReceiptItem): ReceiptItem = Milk(item)
+    }
 }
 
 class Sugar(private val item: ReceiptItem) : ReceiptItem {
     override fun getName(): String = item.getName() + " + sugar"
     override fun getPrice(): Double = item.getPrice() + 0.2
+
+    companion object : DecoratorFactory {
+        override fun decorate(item: ReceiptItem): ReceiptItem = Sugar(item)
+    }
 }
 
 class Tea : ReceiptItem {
